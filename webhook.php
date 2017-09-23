ccbb38da549abf41a50b395bd2b039b4a485b5ff
thongdam <thongdamlll@gmail.com>
2017-09-23 04:32:09 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,4 +1,12 @@
 <?php
+
+$json = file_get_contents('https://obscure-tundra-57006.herokuapp.com/webhook.php');
+$opj = json_decode($json);
+print_r($opj);
+
+
+
+
 $access_token = '17PM+HYIz7F2ncilultvHjsI/WgZJAgwTPEsWRw58pAiO73LDeqbwRizOP9nf2J0ctpMJukP8X6r4LLs0M2ILdVl660EdacLD5Q6xQmEHc4z3FtUXYUzn9qqhUQpHPM2iWCaqTOjI4/FEJQqVr2LbQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
