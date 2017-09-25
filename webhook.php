4aad4e4ec987fffa5dd5aa664278334c516c371a
thongdam <thongdamlll@gmail.com>
2017-09-25 10:20:29 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,3 +1,8 @@
 <?php 
-   $line_api =  $_POST["https://obscure-tundra-57006.herokuapp.com/webhook.php"];
- print_r($line_api);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('17PM+HYIz7F2ncilultvHjsI/WgZJAgwTPEsWRw58pAiO73LDeqbwRizOP9nf2J0ctpMJukP8X6r4LLs0M2ILdVl660EdacLD5Q6xQmEHc4z3FtUXYUzn9qqhUQpHPM2iWCaqTOjI4/FEJQqVr2LbQdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '0343cb56233dac2308244ab6f895204b']);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->replyMessage('<replyToken>', $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
