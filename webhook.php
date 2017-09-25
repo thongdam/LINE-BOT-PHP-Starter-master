6a86461f4dac3d6ac30659fd7e2b9eb4ca5346b0
thongdam <thongdamlll@gmail.com>
2017-09-25 17:12:16 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -22,7 +22,7 @@ if (!is_null($events['events'])) {
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
+			$url = 'https://thongdam.com/create';
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
