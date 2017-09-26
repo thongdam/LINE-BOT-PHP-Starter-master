760823a870e06468d092c25b60dc96cb7c84abf6
thongdam <thongdamlll@gmail.com>
2017-09-26 10:21:56 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -26,17 +26,11 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://thongdam.com/create';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => $text,
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
