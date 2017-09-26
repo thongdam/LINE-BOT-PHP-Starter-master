3e24776cd971f3fd37797a390713e00a3b0e23b0
thongdam <thongdamlll@gmail.com>
2017-09-26 10:16:06 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -10,6 +10,12 @@
   <body>
 <?php
 $access_token = '17PM+HYIz7F2ncilultvHjsI/WgZJAgwTPEsWRw58pAiO73LDeqbwRizOP9nf2J0ctpMJukP8X6r4LLs0M2ILdVl660EdacLD5Q6xQmEHc4z3FtUXYUzn9qqhUQpHPM2iWCaqTOjI4/FEJQqVr2LbQdB04t89/1O/w1cDnyilFU=';
+
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
