c72301ad93564076130cb01d6131600bc98e2cd0
thongdam <thongdamlll@gmail.com>
2017-09-26 10:28:45 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -26,7 +26,7 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			//Get userID
-			$userID = $event['userID'];
+			$userID = $event['source']['userID'];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://thongdam.com/create';
